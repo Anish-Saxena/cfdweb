79b437b813ef9f09419d3e18143714726458b882
Anish Saxena <anishs@iitk.ac.in>
2018-03-15 18:31:12 +0000

    .

--- SampleParse/index.js
+++ SampleParse/index.js
@@ -2,5 +2,5 @@ var PDFJS=require('pdfjs-dist');
 var fs = require('fs');
 var data = new Uint8Array(fs.readFileSync('latex_project.pdf'));
 PDFJS.getDocument(data).then(function (pdfDocument) {
-  console.log('Contents:\n' + pdfDocument.numPages);
+  console.log('Number of pages: ' + pdfDocument.numPages);
 });
